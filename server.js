22f8af00047e88b39663100c76cfaa75caa05f75
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-10-20 00:33:04 +0530

    adding body parser

--- server.js
+++ server.js
@@ -13,3 +13,21 @@ const app = express();
 // this going to give   express access to angular
 
 app.use(express.static(path.join(__dirname,'dist')));
+
+
+// body parser
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use('/api',api);
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'dist/index.html'));
+});
+
+app.listen(port,function()
+{
+  console.log("Server running on localhost:" + port);
+})
+
