842cdf23691b6b2e23be41e7af1c8428df74ecb1
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-10-20 19:10:45 +0530

    making videoCenter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { VideoCenterComponent } from './video-center/video-center.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    VideoCenterComponent
   ],
   imports: [
     BrowserModule,
