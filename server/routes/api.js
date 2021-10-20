b5e96906a65a5cfe4fcbccc72f34134e4bd72a8b
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-10-20 18:17:42 +0530

    put request

--- server/routes/api.js
+++ server/routes/api.js
@@ -68,5 +68,28 @@ router.post('./video',function(req,res){
     })
 })
 
+// put request
 
+
+router.put('/video/:id',function(req,res)
+   {
+      console.log('update a video');
+      Video.findByIdAndUpdate(req.params.id,
+        {
+          $set:{title:req.body.title, url:req.body.url, description:req.body.description}
+      },
+      {
+          new:true
+      },
+      function(err,updatedVideo)
+      {
+          if (err)
+          {
+              res.send("error updating video");
+          }else{
+              res.json(updatedVideo);
+          }
+      }
+      )
+  })
 module.exports = router;
