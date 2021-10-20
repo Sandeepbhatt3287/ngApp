1d0f69113e7ff7975e38c00eecf74b7b4f90f253
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-10-20 15:58:10 +0530

    getting data from mongodb

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const mongoose= require('mongoose');
+
+const Video = require('../models/video');
  
 // connecting to the database
 
@@ -15,9 +17,19 @@ db.on('error',console.error.bind(console,'error connecting to db'));
 
 
 
-router.get('/',function(req,res)
+router.get('/videos',function(req,res)
 {
-    res.send('api works');
+    // res.send('api works');
+    console.log('get reuest for all videos');
+    Video.find({})
+    .exec(function(err,videos)
+    {
+        if (err){
+            console.log("ERROR!!! retrieving vidoes");
+        }else{
+            res.json(vidoes);
+        }
+    })
 });
 
 module.exports = router;
