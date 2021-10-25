2e40eaa569571161594a21fce302c1cf0600ae1d
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-10-25 12:15:33 +0530

    select video event

--- src/app/video-center/video-center.component.ts
+++ src/app/video-center/video-center.component.ts
@@ -9,15 +9,22 @@ import { Video } from '../video';
 export class VideoCenterComponent implements OnInit {
   videos: Video[]=[
 
-    {"_id":"1","title":"title 1","url":"url 1","description":"desc 1"},
-    {"_id":"1","title":"title 2","url":"url 1","description":"desc 1"},
-    {"_id":"1","title":"title 3","url":"url 1","description":"desc 1"}
+    {"_id":"1","title":"Title 1","url":"url 1","description":"desc 1"},
+    {"_id":"2","title":"Title 2","url":"url 2","description":"desc 2"},
+    {"_id":"3","title":"Title 3","url":"url 3","description":"desc 3"},
+    {"_id":"4","title":"Title 4","url":"url 4","description":"desc 4"}
+                ];
 
-  ];
+  selectedVideo!: Video;
 
   constructor() { }
-
+ 
   ngOnInit(): void {
   }
-
+   
+  onSelectedVideo(video:any)
+  {
+    this.selectedVideo= video;
+    console.log(this.selectedVideo);
+  }
 }
