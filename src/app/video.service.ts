f66f6a69ec5cd3a4e14eb2d37ebd6372cb30ac21
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-10-25 20:19:59 +0530

    api is not working

---
+++ src/app/video.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpClientModule} from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VideoService {
+
+  public _getUrl = './api/videos';
+
+  constructor( public _http: HttpClient) { }
+
+  getVideos()
+  {
+    return this._http.get(this._getUrl)
+    .pipe(map((res:any) => res.json()));
+  }
+
+}
+
+// function videos(videos: any): any {
+  // throw new Error('Function not implemented.');
+// }
+  // function tap(arg0: (data: any) => void): import("rxjs").OperatorFunction<unknown, unknown> {
+  //   throw new Error('Function not implemented.');
+  // }
+
